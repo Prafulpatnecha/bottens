8bab6be3bfed284e6933ac39fed2ae0d248aebd2
Praful Patnecha <prafulpatnecha@gmail.com>
2024-04-05 13:58:41 +0530

    5-4-24

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+
       title: 'An Indian Flag',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
